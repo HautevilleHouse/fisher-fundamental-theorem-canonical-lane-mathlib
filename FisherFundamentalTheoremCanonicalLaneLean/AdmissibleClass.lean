c860bd6ef6634b3502cad2384762ce9d028fa626
HautevilleHouse <239203806+HautevilleHouse@users.noreply.github.com>
2026-07-29 04:49:17 -0400

    Canon

---
+++ FisherFundamentalTheoremCanonicalLaneLean/AdmissibleClass.lean
@@ -0,0 +1,16 @@
+import FisherFundamentalTheoremCanonicalLaneLean.MathlibObjects
+
+namespace HautevilleHouse
+namespace FisherFundamentalTheoremCanonicalLaneLean
+
+structure AdmissibleClass where
+  object : FisherAdmittedObject
+  endpointSatisfied : Prop
+  remainderRecorded : Prop
+  gateWitness : endpointSatisfied ∨ remainderRecorded
+
+def admittedClosure (A : AdmissibleClass) : Prop :=
+  FisherWitnessClosed A.object ∧ (A.endpointSatisfied ∨ A.remainderRecorded)
+
+end FisherFundamentalTheoremCanonicalLaneLean
+end HautevilleHouse
